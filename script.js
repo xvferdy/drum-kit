10fe4c6c441fca9542f551700e7ff85cc79fdfa5
ferdy <xv.ferdy@gmail.com>
2019-03-31 10:04:12 +0700

    Add keydown event, aading comments

--- script.js
+++ script.js
@@ -1,62 +1,70 @@
-function makeSound(key) {
+//variabel untuk menentukan document object
+var button = document.querySelectorAll("button");
+var buttonList = document.querySelectorAll("button").length
 
+//untuk membuat suara
+function makeSound(key) {
     switch (key) {
         case "v":
-            var snare = new Audio("sounds/Snare 5.mp3");
+            const snare = new Audio("sounds/Snare 5.wav");
             snare.play();
             break;
         case "b":
-            var kick = new Audio("sounds/Kick 4.mp3");
+            const kick = new Audio("sounds/Kick 4.mp3");
             kick.play();
             break;
         case "n":
-            var bass = new Audio("sounds/Bass 6 (C).mp3");
+            const bass = new Audio("sounds/Bass 6 (C).mp3");
             bass.play();
             break;
         case "m":
-            var i808 = new Audio("sounds/808 1 (C).mp3");
+            const i808 = new Audio("sounds/808 1 (C).mp3");
             i808.play();
             break;
         case "j":
-            var chant = new Audio("sounds/Chant 5.mp3");
+            const chant = new Audio("sounds/Chant 5.mp3");
             chant.play();
             break;
         case "k":
-            var fx = new Audio("sounds/FX 5.mp3");
+            const fx = new Audio("sounds/FX 5.mp3");
             fx.play();
             break;
         case "l":
-            var perc = new Audio("sounds/Perc 3.mp3");
+            const perc = new Audio("sounds/Perc 3.mp3");
             perc.play();
             break;
         case "u":
-            var hat = new Audio("sounds/Hat 16.mp3");
+            const hat = new Audio("sounds/Hat 16.mp3");
             hat.play();
             break;
         case "i":
-            var clap = new Audio("sounds/Clap 14.wav");
+            const clap = new Audio("sounds/Clap 14.mp3");
             clap.play();
             break;
         case "o":
-            var ride = new Audio("sounds/Ride.wav");
+            const ride = new Audio("sounds/Ride.mp3");
             ride.play();
             break;
         case "p":
-            var open = new Audio("sounds/Open Hat 5.wav");
+            const open = new Audio("sounds/Open Hat 5.mp3");
             open.play();
             break;
 
         default:
             console.log(this);
     }
-
 }
 
-//make var
-var button = document.querySelectorAll("button");
-var buttonList = document.querySelectorAll("button").length
+//untuk animasi flash pada tombol
+function buttonAnimation(event) {
+    var activeButton = document.querySelector("." + event);
+    activeButton.classList.add('playing');
+    setTimeout(function () {
+        activeButton.classList.remove('playing');
+    }, 100);
+}
 
-//run event "click" on mouse
+//event untuk masing2 tombol dikilik
 for (i = 0; i < buttonList; i++) {
     button[i].addEventListener("click", function () {
         var buttonClass = this.className;
@@ -65,12 +73,9 @@ for (i = 0; i < buttonList; i++) {
     });
 }
 
-
-//untuk animasi flash
-function buttonAnimation(event) {
-    var activeButton = document.querySelector("." + event);
-    activeButton.classList.add('playing');
-    setTimeout(function () {
-        activeButton.classList.remove('playing');
-    }, 100);
-}
+//event untuk masing2 tombol ditekan
+//event object
+document.addEventListener("keydown", function (event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+})
